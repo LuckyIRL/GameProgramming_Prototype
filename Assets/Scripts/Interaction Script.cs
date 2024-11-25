681bbc683970683208b98a006a09e5b79031034c
LuckyIRL <C23775565@mytudublin.ie>
2024-11-25 18:19:27 +0000

    Push

---
+++ Assets/Scripts/Interaction Script.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractionScript : MonoBehaviour
+{
+    public UnityEvent enteredTrigger, exitedTrigger, interacted;
+
+    private bool insideTrigger;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            enteredTrigger.Invoke();
+            insideTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            exitedTrigger.Invoke();
+            insideTrigger = false;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (insideTrigger && Input.GetKeyDown(KeyCode.E))
+        {
+            exitedTrigger.Invoke();
+            interacted.Invoke();
+        }
+    }
+}
